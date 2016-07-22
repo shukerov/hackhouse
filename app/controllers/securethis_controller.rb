b8bfedc1476870e8d6cf3a883b6c087fb27ed702
Taylor Thurlow <taylorthurlow8@gmail.com>
2016-07-22 00:04:37 -0700

    Added users, password, signup. Partly works. probably. maybe.

---
+++ app/controllers/securethis_controller.rb
@@ -0,0 +1,9 @@
+class SecurethisController < ApplicationController
+	
+	before_filter :authorize
+
+	def new
+
+	end
+
+end
---
+++ test/controllers/securethis_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SecurethisControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
